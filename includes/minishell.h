42f470165d4c364dacb64ad86de1114aff0844b1
Ilnur Sultanov <isultanov99@gmail.com>
2021-08-11 12:53:39 +0300

    moving

--- includes/minishell.h
+++ includes/minishell.h
@@ -2,6 +2,24 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <signal.h>
 #include "../libft/libft.h"
 #include <readline/readline.h>
 #include <readline/history.h>
+
+typedef struct s_vars{
+ char   **envp_copy;
+ int    count_envp;
+ int    save_std_in;
+ int    save_std_out;
+ int    count_pipe;
+ int    count_command;
+ int    index_pipe;
+ int    count_redirect;
+ int    count_call_pipe;
+ int    flag_redirect;
+ int    err_flag;
+ int    err_flag_export;
+ int    cd_flag;
+ int    *arr_flags;
+}    t_vars;
--- srcs/main.c
+++ srcs/main.c
@@ -1,23 +1,37 @@
 #include "../includes/minishell.h"
 
+int	count_words(char* readline)
+{
+	int i;
+	int wordcount;
+
+	i = 0;
+	wordcount = 0;
+	while (readline[i] != '\n')
+	{
+		if (readline[i] != ' ' && (readline[i+1] == ' ' || readline[i+1] == '\n'))
+			wordcount++;
+		i++;
+	}
+	return (wordcount);
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	char* cwd;
 	char* line;
-	char* usernm;
 
-	cwd = ft_calloc(1024, 1);
-	getcwd(cwd, 1024);
-	usernm = ft_strjoin(getenv("USER"),":");
-	write(1, "\033[1;32m", ft_strlen("\033[1;31m"));
-	write(1, usernm, ft_strlen(usernm));
-	write(1, "\033[1;34m", ft_strlen("\033[1;34m"));
-	write(1, cwd, ft_strlen(cwd));
-	line = readline("\033[0m$ ");
-
-	printf("%s", line);
-	free(cwd);
-	free(line);
-	free(usernm);
+	signal(SIGQUIT, )
+	while (1)
+	{
+		cwd = ft_calloc(1024, 1);
+		getcwd(cwd, 1024);
+		write(1, "\033[1;34m", ft_strlen("\033[1;34m"));
+		line = readline(cwd);
+		write(1, "\033[0m", ft_strlen("\033[0m"));
+		add_history(line);
+		free(line);
+		free(cwd);
+	}
 	return (0);
 }
