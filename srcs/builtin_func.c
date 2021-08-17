8a17a7a813c2df43a4b04bb9966ea08d3b69c233
Chartreuse Pablo <cpablo@il-d3.msk.21-school.ru>
2021-08-17 19:27:04 +0300

    cd-echo

--- srcs/builtin_func.c
+++ srcs/builtin_func.c
@@ -29,11 +29,51 @@ void	echo(char **line)
 	printf("\n");
 }
 
+char			**count_commands(char *line)
+{
+	int		i;
+	int		count;
+	char	**command_line;
+	int		flag;
+
+	command_line = NULL;
+	count = 0;
+	flag = 0;
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if (line[i] != ' ')
+		{
+			count = count + 1;
+			while ((line[i] != ' ' || flag == 1 || flag == 2) && line[i] != '\0')
+			{
+				if (line[i] == '"' || line[i] == '\'')
+				{
+					if (flag == 0 && line[i] == '"')
+						flag = 1;
+					else if (flag == 1 && line[i] == '"')
+						flag = 0;
+					else if (flag == 0 && line[i] == '\'')
+						flag = 2;
+					else if (flag == 2 && line[i] == '\'')
+						flag = 0;
+				}
+				i = i + 1;
+			}
+		}
+		else
+			i = i + 1;
+	}
+	command_line = malloc(sizeof(char *) * (count + 1));
+	return (command_line);
+}
+
 int main(int argc, char **argv)
 {
 	char **command_line;
 
-	command_line = 'авыаыа';
+	command_line = malloc(sizeof(char *) * 100);
+	command_line[0] = "авыаыа";
 	cd("/Users/arsenyburov/Coding/");
 	pwd();
 	echo(&command_line[0]);
